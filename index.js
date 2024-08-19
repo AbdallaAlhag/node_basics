adf46406a66d52a0cc3194bd73093061b156388f
Abdalla Alhag <alhagabdalla@gmail.com>
2024-08-19 14:19:24 -0700

    Add server and pages

---
+++ index.js
@@ -0,0 +1,61 @@
+import http from 'http';
+import fs from 'fs';
+
+const server = http.createServer((req, res) => {
+  // console.log(req);
+  console.log(req.url);
+
+  // set header content type
+  res.setHeader('Content-Type', 'text/html');
+
+  // res.write('<p>hello, ninjas</p>');
+  // res.write('<p>hello again, ninjas</p>');
+  // res.end();
+
+  // send html file
+  // fs.readFile('./views/index.html', (err, data) => {
+  //   if (err) {
+  //     console.log(err);
+  //     res.end();
+  //   }
+  //   //res.write(data);
+  //   res.end(data);
+  // });
+
+  // routing
+  let path = './';
+  switch(req.url) {
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200;
+      break;
+    case '/about':
+      path += 'about.html';
+      res.statusCode = 200;
+      break;
+    case '/contact-me':
+        path += 'contact-me.html';
+        res.statusCode = 200;
+        break;
+    default:
+      path += '404.html';
+      res.statusCode = 404;
+  }
+
+  // send html
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    }
+    //res.write(data);
+    res.end(data);
+  });
+
+
+});
+
+// localhost is the default value for 2nd argument
+server.listen(3000, 'localhost', () => {
+  console.log('listening for requests on port 3000');
+});
